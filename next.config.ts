7e03cbc242220843d394fc6d18ad4ddd63ae81be
deadlockanalysis <abbeybolton04@gmail.com>
2026-03-07 19:32:49 -0500

    Remove oversized hero image tracing from serverless bundle

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingIncludes: {
-    "/api/hero-images/**": ["./deadlock_hero_images/**/*"],
-    "/api/hero-images/[heroFolder]/[fileName]": ["./deadlock_hero_images/**/*"],
-    "/api/hero-images/[heroFolder]/[fileName]/route": ["./deadlock_hero_images/**/*"],
     "/api/item-icons/**": ["./deadlock_icons/**/*"],
     "/api/item-icons/[fileName]": ["./deadlock_icons/**/*"],
     "/api/item-icons/[fileName]/route": ["./deadlock_icons/**/*"],
