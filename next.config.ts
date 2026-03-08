81fb4d68e0a53ba23d10226e0535aba49fc38dd3
deadlockanalysis <abbeybolton04@gmail.com>
2026-03-07 19:03:19 -0500

    Scope image tracing to image API routes only

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/api/hero-images/[heroFolder]/[fileName]": ["./deadlock_hero_images/**/*"],
+    "/api/item-icons/[fileName]": ["./deadlock_icons/**/*"],
+  },
   outputFileTracingExcludes: {
-    "*": [
+    "/*": [
+      "./deadlock_hero_images/**/*",
+      "./deadlock_icons/**/*",
+      "./hero_portraits/**/*",
       "./reports/**/*",
     ],
   },
