9eb6f0a44a0101d8a78d3aab3168fd351bf636ff
deadlockanalysis <abbeybolton04@gmail.com>
2026-03-20 20:43:45 -0400

    fix: exclude non-render hero images specifically so _Render.png is available on Vercel

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,14 @@ const nextConfig: NextConfig = {
   },
   outputFileTracingExcludes: {
     "*": [
-      "./deadlock_hero_images/**/*",
+      "./deadlock_hero_images/**/*.webp",
+      "./deadlock_hero_images/**/background_image.png",
+      "./deadlock_hero_images/**/hero_card_*.png",
+      "./deadlock_hero_images/**/icon_hero_card.png",
+      "./deadlock_hero_images/**/icon_image_small.png",
+      "./deadlock_hero_images/**/minimap_image.png",
+      "./deadlock_hero_images/**/top_bar_vertical_image.png",
+      "./deadlock_hero_images/**/name_image",
       "./deadlock_icons/**/*",
       "./hero_portraits/**/*",
       "./reports/**/*",
